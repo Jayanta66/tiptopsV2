3949022a9cd1577a43094657889dec6319f2c204
Jayanta66 <jayhalder18@gmail.com>
2025-01-16 09:39:35 +0530

    add

---
+++ src/main/java/com/example/tiptop/TiptopApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiptopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiptopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tiptop/TiptopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiptopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
